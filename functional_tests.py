42814d322cf84b44c23208f2d4da1e431490710a
Matthew Higgins <mrhiggi@g.clemson.edu>
2017-12-21 12:23:27 -0500

    refactored ft

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         self.browser.get('http://localhost:8000')
@@ -29,18 +34,17 @@ class NewVisitorTest(unittest.TestCase):
         #"1: The ting we type" in the list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
-        )
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         #text box still present for next item
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
         #page updates showing new item added
 
+        self.fail('Finish the test!')
         #unique url for the list
 
         #visit unique url, list is there
--- lists/tests.py
+++ lists/tests.py
@@ -14,3 +14,9 @@ class HomePageTest(TestCase):
         html = response.content.decode('utf8')
 
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+
+        self.assertTemplateUsed(response, 'home.html')
