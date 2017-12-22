d005b5ea30b1cc2ad2a32f948d9c80afc3a89112
Matthew Higgins <mrhiggi@g.clemson.edu>
2017-12-22 14:02:24 -0500

    created base.css and refactored secret out of settings.py

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 import time
 import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     MAX_WAIT = 10
 
