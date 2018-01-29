5e34de23cd4babba2488566cfe84fdcc32451a14
Matthew Higgins <mrhiggi@g.clemson.edu>
2018-01-29 16:18:40 -0500

    absolute url was not the missing test, fixed now)

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -9,6 +9,10 @@ from lists.models import Item, List
 # Create your tests here.
 class HomePageTest(TestCase):
 
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+    
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')
 
