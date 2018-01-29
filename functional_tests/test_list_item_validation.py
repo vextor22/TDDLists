74510587a13634fec81724d9b9f33b1a17f1bc8d
Matthew Higgins <mrhiggi@g.clemson.edu>
2018-01-29 17:22:50 -0500

    Enforcing model validation in list view

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -24,6 +24,7 @@ class ItemValidationTest(FunctionalTest):
         self.wait_for_row_in_list_table('1: Buy milk')
 
 
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         #similar warning message
         self.wait_for(lambda: self.assertEqual(
--- lists/views.py
+++ lists/views.py
@@ -10,11 +10,17 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
+    error = None
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list=list_)
-        return redirect(f'/lists/{list_.id}/')
+        try:
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(f'/lists/{list_.id}/')
+        except ValidationError:
+            error = "You can't have an empty list item"
     items = Item.objects.filter(list=list_)
-    return render(request, 'list.html', {'items': items, 'list_id': list_id})
+    return render(request, 'list.html', {'items': items, 'list_id': list_id, 'error': error})
 #    return render(request, 'list.html', {'list': list_})
 
 def new_list(request):
@@ -31,7 +37,3 @@ def new_list(request):
         return render(request, 'home.html', {"error": error})
     return redirect(f'/lists/{list_.id}/')
 
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
